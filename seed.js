c3c9032d118b6d2fe3d7a4141bef09ccb27e32e6
abby wigdale <abby.wigdale@gmail.com>
2018-08-06 16:36:09 -0500

    we made veggies

--- seed.js
+++ seed.js
@@ -5,10 +5,37 @@ models.db.sync({
 })
 .then(() => {
   console.log('this works');
-  models.db.close();
+  return models.Vegetable.bulkCreate([{
+    name: 'carrot',
+    color: 'orange',
+    planted_on: Date.now()
+  }, {name: 'lettuce', color: 'green', planted_on: Date.now()},
+  {name: 'radish', color: 'red', planted_on: Date.now()}])
 })
 .catch(err => {
   console.log(err);
+})
+.finally(() => {
   models.db.close();
 })
 
+// function makeGardener(name, age){
+//   const gardener = new models.Gardener({
+//     name: name,
+//     age: age
+//   })
+//   // const newGardeners = gardener;
+//   // newGardeners.save();
+// }
+
+// const newGardener = gardener.then(() => console.log(gardener))
+
+// function makeVegetables(name, color){
+//   const newVeggie = models.Vegetable.create({
+//     name: name,
+//     color: color,
+//     planted_on: Date.now()
+//   })
+// }
+
+// makeVegetables('carrot', 'orange');
